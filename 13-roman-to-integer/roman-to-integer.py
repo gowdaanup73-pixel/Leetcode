81ffd52f38fd8b6b67d827cb0b5d2d63f7fedcd2
Anup Gowda K T <227783393+gowdaanup73-pixel@users.noreply.github.com>
2026-09-10 19:47:15 +0530

    Update roman-to-integer.py

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -5,6 +5,7 @@ class Solution:
         for i in range(len(s)):
             if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:                
                 total -= values[s[i]]
+                
             else:
                 total += values[s[i]]
         return total
